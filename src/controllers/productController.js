3184bf8c1b7cea07fefeaf38a2d9530dc2d193f5
jflukac <juanfranciscolukac@gmail.com>
2020-12-16 12:47:15 -0300

    Modify routes system to new folders

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,15 +1,15 @@
 const controladorProductos = {
     productDetail: function(req, res) {
-      res.render('productDetail');
+      res.render('./products/productDetail');
     },
     productCart: function (req,res) {
-      res.render('productCart')
+      res.render('./products/productCart')
     },
     productCreate: (req, res) => {
-      res.render('productCreate')
+      res.render('./products/productCreate')
     },
     productEdit: (req, res) => {
-      res.render ('productEdit')
+      res.render ('./products/productEdit')
     },
 }
 
