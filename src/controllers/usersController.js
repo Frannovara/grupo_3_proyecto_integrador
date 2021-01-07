65eb8906dc7310e8f6b20cd65e89c99068279fc1
lautaro porlan <l.porlann@gmail.com>
2021-01-07 11:25:39 -0300

    users controller const and path added

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,18 @@
 const { render } = require("ejs");
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+
 
 const controladorUsuarios = {
     login: (req , res) =>{
         res.render ("./users/login")
     },
+    
+    
     register: (req , res) =>{
         res.render ("./users/register")
     },
