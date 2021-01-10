960e049f8a5fa706a1d8ed9783f4d3a3687aa5e4
lautaro porlan <l.porlann@gmail.com>
2021-01-10 19:06:08 -0300

    login middleware modified

--- src/middlewares/logInValidator.js
+++ src/middlewares/logInValidator.js
@@ -3,7 +3,7 @@ var {check} = require ('express-validator')
 
 
 module.exports = 
-    [check('email').isEmail().withMessage('El email debe ser valido'),
+    [check('email').isEmail().withMessage('El email no se encuentra registrado'),
 
     check('password').isLength({
         min: 8
