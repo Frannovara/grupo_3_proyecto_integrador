d8d87ac2b71b68b24d938c85b1bb9a3eabaf99a2
jflukac <juanfranciscolukac@gmail.com>
2021-01-27 20:00:32 -0300

    sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,7 +4,8 @@ module.exports = {
     "password": null,
     "database": "database_development",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "operatorsAliases": false
   },
   "test": {
     "username": "root",
