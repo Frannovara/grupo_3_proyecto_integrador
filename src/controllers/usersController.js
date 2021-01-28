6fdcaf98620ea031fd68b94186d9d4a8b64bedc8
jflukac <juanfranciscolukac@gmail.com>
2021-01-27 21:40:17 -0300

    user controller beautify

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,12 @@
-const { render } = require("ejs");
+const {
+    render
+} = require("ejs");
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcryptjs');
-const {  validationResult  } = require('express-validator');
+const {
+    validationResult
+} = require('express-validator');
 const nodemailer = require('nodemailer');
 
 
@@ -10,14 +14,14 @@ const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 function makeid() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&?¡¿!';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&?¡¿!';
     var charactersLength = characters.length;
-    for ( var i = 0; i < 10; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (var i = 0; i < 10; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
- }
+}
 
 // Función que envía un mail para regenerar la contraseña
 async function newPass(req) {
@@ -53,25 +57,25 @@ async function newPass(req) {
     users.forEach((user) => {
         if (user.email == req.body.email) {
             user.password = bcrypt.hashSync(randomPassword, 10)
-        const usersJson = JSON.stringify(users)
-        fs.writeFileSync(usersFilePath, usersJson)
+            const usersJson = JSON.stringify(users)
+            fs.writeFileSync(usersFilePath, usersJson)
         }
     })
 }
 
-const saveUser = function(req) {
-	let newUser = {
-		id: Date.now(),
+const saveUser = function (req) {
+    let newUser = {
+        id: Date.now(),
         ...req.body,
         image: "/images/users/default.png",
         category: 'user'
     };
-      
-    
+
+
     delete newUser.terms
     newUser.password = bcrypt.hashSync(newUser.password, 10);
-	users.push(newUser);
-	let usersJSON = JSON.stringify(users);
+    users.push(newUser);
+    let usersJSON = JSON.stringify(users);
     fs.writeFileSync(usersFilePath, usersJSON)
     console.log(newUser)
 }
@@ -84,24 +88,24 @@ const setImage = (req) => {
             return user.id
         }
     })
-    
+
 }
 
-const editUser =  (req) => {
-	users.forEach((user) => {
-		if (user.id == req.params.id) {
-			// product = { ...product, ...req.body, image: req.files[0].filename }
-			user.first_name = req.body.first_name
+const editUser = (req) => {
+    users.forEach((user) => {
+        if (user.id == req.params.id) {
+            // product = { ...product, ...req.body, image: req.files[0].filename }
+            user.first_name = req.body.first_name
             user.last_name = req.body.last_name
             user.email = req.body.email
             if (req.body.password != '') {
                 user.password = bcrypt.hashSync(req.body.password, 10)
-            };            
+            };
             user.category = req.body.category
             const usersJson = JSON.stringify(users)
             fs.writeFileSync(usersFilePath, usersJson)
-		}
-	})
+        }
+    })
 }
 
 const deleteUser = (req) => {
@@ -111,42 +115,52 @@ const deleteUser = (req) => {
 }
 
 const controladorUsuarios = {
-    login: (req , res) =>{
-        res.render ('./users/login' ,  {loginData : {} , errors : []})
+    login: (req, res) => {
+        res.render('./users/login', {
+            loginData: {},
+            errors: []
+        })
     },
-    loginProcess: (req , res) =>{
+    loginProcess: (req, res) => {
         let errors = validationResult(req)
-        console.log (errors)
-        if (errors.isEmpty()){
-            let userToLogin = {...users.find(user => user.email === req.body.email)}
-            console.log(userToLogin)
-            if( userToLogin != undefined ){
-                if( bcrypt.compareSync(req.body.password , userToLogin.password)  ){
-                        delete userToLogin.password;
-                        req.session.user = userToLogin
-                         if (req.body.remember){res.cookie('user' ,userToLogin,{maxAge: 1000 * 60 * 60} )}
-                         res.locals.usuario = req.session.user
-                        res.redirect('/users/profile');//como sabe que perfil es???
-                    }else{
-                        res.render ('./users/login' , {errors: errors.mapped() , loginData : {...req.body, password: ''}})
+        if (errors.isEmpty()) {
+            let userToLogin = {
+                ...users.find(user => user.email === req.body.email)
+            }
+            if (userToLogin != undefined) {
+                if (bcrypt.compareSync(req.body.password, userToLogin.password)) {
+                    delete userToLogin.password;
+                    req.session.user = userToLogin
+                    if (req.body.remember) {
+                        res.cookie('user', userToLogin, {
+                            maxAge: 1000 * 60 * 60
+                        })
+                    }
+                    res.locals.usuario = req.session.user
+                    res.redirect('/users/profile'); //como sabe que perfil es???
                 }
-            }else{
-                res.render ('./users/login' , {errors: errors.mapped() , loginData : {...req.body, password: ''}})
-            }   
-        }else{
-            res.render ('./users/login' , {errors: errors.mapped() , loginData : {...req.body, password: ''}});
-       }
+            }
+        }
+        res.render('./users/login', {
+            errors: errors.mapped(),
+            loginData: {
+                ...req.body,
+                password: ''
+            }
+        });
     },
     logout: (req, res) => {
         req.session.destroy();
         res.redirect('/')
     },
-    register: (req , res) =>{
-        res.render ('./users/register')
+    register: (req, res) => {
+        res.render('./users/register')
     },
     profile: (req, res) => {
         const user = req.session.user
-        res.render('./users/profile', {user})
+        res.render('./users/profile', {
+            user
+        })
     },
     saveUser: (req, res) => {
         saveUser(req)
@@ -174,6 +188,6 @@ const controladorUsuarios = {
         res.redirect('/')
     }
 }
-    
+
 
 module.exports = controladorUsuarios;
