f05980eee9bee57cd95d9cf346cfd5f78ed0b578
lautaro porlan <l.porlann@gmail.com>
2021-02-01 21:42:56 -0300

    product model created

---
+++ src/database/models/Product.js
@@ -0,0 +1,56 @@
+ module.exports = (sequelize , dataTypes) =>{
+
+    let alias = 'Products';
+    let cols = {
+        id : {
+            autoIncrement : true,
+            primaryKey : true,
+            type : dataTypes.BIGINT,
+            allowNull : false
+        },
+        name : {
+            type : dataTypes.STRING,
+            allowNull : false
+        },
+        description : {
+            type : dataTypes.STRING,
+            allowNull : false
+        },
+        year : {
+            type : dataTypes.INTEGER,
+            allowNull : false
+        },
+        base_price: {
+            type : dataTypes.DECIMAL,
+            allowNull : false
+        },
+        discount: {
+            type : dataTypes.INTEGER, 
+        },
+        final_price: {
+            type : dataTypes.DECIMAL,
+        },
+        created_at: {
+            type: dataTypes.DATE,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+        },
+        deleted_at: {
+            type: dataTypes.DATE,
+        },
+        category_id: {
+            type: dataTypes.BIGINT,
+        }
+    }
+    let config = {
+        tableName: 'products',
+        timestamps: true,
+        creadetAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAT: 'deleted_at'
+    }
+    const Product = sequelize.define(alias, cols, config);
+
+    return Product;
+} 
