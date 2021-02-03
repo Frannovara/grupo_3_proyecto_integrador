4d9d2ae2eb2df40c3dd688473d33d06d4abd7900
lautaro porlan <l.porlann@gmail.com>
2021-02-02 21:27:50 -0300

    brand js model created

---
+++ src/database/models/Brand.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize , dataTypes) =>{
+    let alias = 'Brands';
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.BIGINT,
+            allowNull:false
+        },
+        created_at: {
+            type: dataTypes.DATE,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+        },
+        deleted_at: {
+            type: dataTypes.DATE,
+        },
+
+        name: {
+            type: dataTypes.STRING,
+            allowNull : false
+        }
+
+    }
+    let config = {
+        tableName: 'brands',
+        timestamps: true,
+        creadetAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at'
+    }
+    const Brand = sequelize.define(alias, cols, config);
+
+    return Brand;
+}
