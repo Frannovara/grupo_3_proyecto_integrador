30891db9dbdbcf17d3e92b336f808a78b1105fdb
lautaro porlan <l.porlann@gmail.com>
2021-02-02 21:29:16 -0300

    Product_category updtd

---
+++ src/database/models/Product_category.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize , dataTypes) =>{
+    let alias = "Product_categories"
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.BIGINT,
+            allowNull:false
+        },
+        created_at: {
+            type: dataTypes.DATE,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+        },
+        deleted_at: {
+            type: dataTypes.DATE,
+        },
+        category: {
+            type: dataTypes.STRING,
+        }
+    }
+
+    let config = {
+        tableName: 'product_categories',
+        timestamps: true,
+        creadetAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at'
+    }
+    const Product_category = sequelize.define(alias, cols, config);
+
+
+    return Product_category;
+}
