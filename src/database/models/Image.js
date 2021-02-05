7f3f9f9d0117eb7e86e4f97bfc43c3db6980f020
Frannovara <you@example.com>
2021-02-05 17:42:08 -0300

    se corrige nombre de model

---
+++ src/database/models/Image.js
@@ -0,0 +1,53 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Images";
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.BIGINT,
+            allowNull:false
+        },
+        created_at: {
+            type: dataTypes.DATE,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+        },
+        deleted_at: {
+            type: dataTypes.DATE,
+        },
+        image: {
+            type : dataTypes.STRING,
+            allowNull : false
+        },
+        product_id: {
+            type: dataTypes.BIGINT,
+        },
+        color_id: {
+            type: dataTypes.BIGINT,
+        }
+    }
+
+    let config = {
+        tableName: 'images',
+        timestamps: true, 
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAT: 'deleted_at'
+    }
+
+    const Images = sequelize.define(alias, cols, config);
+
+    Images.associate = function(models) {
+        Images.belongsTo(models.Colors, {
+            as: 'images_colors',
+            foreignKey: 'color_id'
+        }),
+        Images.hasMany(models.Products, {
+            as: 'images_products',
+            foreignKey: 'product_id'
+        })
+    }
+    return Images;
+}
