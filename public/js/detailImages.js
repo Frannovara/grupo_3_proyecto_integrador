c9c209413d30b73f1b7a22605324a183fa01ed8f
jflukac <juanfranciscolukac@gmail.com>
2021-03-04 18:36:16 -0300

    Change Image with color selection

---
+++ public/js/detailImages.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function() {
+    let qs = function(text) {
+        return document.querySelector(text)
+    }
+    let qsa = function(text) {
+        return document.querySelectorAll(text)
+    }
+
+    let colorButtons = qsa('.dot')
+    let images = []
+    for (let i = 0; i < colorButtons.length; i++) {
+        images[i] = document.getElementById('bike'+i)
+    }
+    for (let i = 0; i < colorButtons.length; i++) {
+        colorButtons[i].addEventListener('click', () => {
+            for (let j = 0; j < images.length; j++) {
+                if (i == j) {
+                    images[j].style.display = 'block'
+                } else {
+                    images[j].style.display = 'none'
+                }      
+            }
+        })
+    }
+    
+})
