df9d03398869d2b99043af1599a5bd53a7a72e30
jflukac <juanfranciscolukac@gmail.com>
2021-03-09 19:15:33 -0300

    createProduct front validation file

---
+++ public/js/createProduct.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+    let qs = function(text) {
+        return document.querySelector(text)
+    }
+    let qsa = function(text) {
+        return document.querySelectorAll(text)
+    }
+
+    let name = qs('#name')
+    let category = qs('#category')
+    let brand = qs('#brand')
+
+})
