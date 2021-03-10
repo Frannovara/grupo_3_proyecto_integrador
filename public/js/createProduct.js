60a8577061977bb68a41875b452fff1d23450f90
jflukac <juanfranciscolukac@gmail.com>
2021-03-10 10:26:24 -0300

    Create Product Validation

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -29,31 +29,86 @@ window.addEventListener('load', function(){
     let colorError = qs('#colorError')
     
     name.addEventListener('blur', function(){
-        
+        if(name.value.length<5){
+            nameError.innerHTML = `<small>El nombre debe tener al menos 5 caracteres</small>`
+            nameError.style.visibility = "visible"
+        } else {
+            nameError.innerHTML = ``
+            nameError.style.visibility = "hidden"
+        }
     })
-    category.addEventListener('blur', function(){
-
+    category.addEventListener('input', function(){
+        if (Number.isInteger(Number(category.value))) {
+            categoryError.innerHTML = ``
+            categoryError.style.visibility = "hidden"
+        } else {
+            categoryError.innerHTML = `<small>Este campo es obligatorio</small>`
+            categoryError.style.visibility = "visible"
+        }
     })
-    brand.addEventListener('blur', function(){
-
+    brand.addEventListener('input', function(){
+        if (Number.isInteger(Number(brand.value))) {
+            brandError.innerHTML = ``
+            brandError.style.visibility = "hidden"
+        } else {
+            brandError.innerHTML = `<small>Este campo es obligatorio</small>`
+            brandError.style.visibility = "visible"
+        }
     })
     price.addEventListener('blur', function(){
-
+        if (Number.isInteger(Number(price.value)) && Number(price.value) > 0) {
+            priceError.innerHTML = ``
+            priceError.style.visibility = "hidden"
+        } else {
+            priceError.innerHTML = `<small>El precio debe ser un número mayor a cero</small>`
+            priceError.style.visibility = "visible"
+        }
     })
     discount.addEventListener('blur', function(){
-
+        if (Number.isInteger(Number(discount.value)) && Number(discount.value) > 0) {
+            discountError.innerHTML = ``
+            discountError.style.visibility = "hidden"
+        } else {
+            discountError.innerHTML = `<small>Debe ingresar un valor de descuento</small>`
+            discountError.style.visibility = "visible"
+        }
     })
     year.addEventListener('blur', function(){
-
+        if (Number.isInteger(Number(year.value)) && year.value.length == 4) {
+            yearError.innerHTML = ``
+            yearError.style.visibility = "hidden"
+        } else {
+            yearError.innerHTML = `<small>Debe ingresar un año en formato YYYY</small>`
+            yearError.style.visibility = "visible"
+        }
     })
     description.addEventListener('blur', function(){
-
+        if(description.value.length <20) {
+            descriptionError.innerHTML = `<small>La descripción debe tener al menos 20 caracteres</small>`
+            descriptionError.style.visibility = "visible"
+        } else {
+            descriptionError.innerHTML = ``
+            descriptionError.style.visibility = "hidden"
+        }
     })
-    image.addEventListener('blur', function(){
-
+    image.addEventListener('input', function(){
+        let imageOk = regexImage.test(image.files[0].type)
+        if(!imageOk) {
+            imageError.innerHTML = `<small>El archivo ingresado no es una imagen</small>`
+            imageError.style.visibility = "visible"
+        } else {
+            imageError.innerHTML = ``
+            imageError.style.visibility = "hidden"
+        }
     })
-    color.addEventListener('blur', function(){
-
+    color.addEventListener('input', function(){
+        if (Number.isInteger(Number(color.value))) {
+            colorError.innerHTML = ``
+            colorError.style.visibility = "hidden"
+        } else {
+            colorError.innerHTML = `<small>Este campo es obligatorio</small>`
+            colorError.style.visibility = "visible"
+        }
     })
 
 
