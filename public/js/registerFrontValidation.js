e84464c19440c32d994a3239602bc91b0d5cc888
jflukac <juanfranciscolukac@gmail.com>
2021-03-10 11:05:53 -0300

    register errores

--- public/js/registerFrontValidation.js
+++ public/js/registerFrontValidation.js
@@ -28,48 +28,61 @@ window.addEventListener('load', function() {
     firstName.addEventListener('blur', function() {
         if(firstName.value.length < 2 ) {
             firstNameError.innerHTML = "<small>El nombre debe tener al menos 2 caracteres</small>"
+            firstNameError.style.visibility = 'visible'
         } else {
             firstNameError.innerHTML = ""
+            firstNameError.style.visibility = 'hidden'
         }
     })
     lastName.addEventListener('blur', function() {
         if(lastName.value.length < 2 ) {
             lastNameError.innerHTML = "<small>El apellido debe tener al menos 2 caracteres</small>"
+            lastNameError.style.visibility = 'visible'
         } else {
             lastNameError.innerHTML = ""
+            lastNameError.style.visibility = 'hidden'
         }
     })
     email.addEventListener('blur', function() {
         let emailOk = regexEmail.test(email.value)
         if(emailOk) {
             emailError.innerHTML = ""
+            emailError.style.visibility = 'hidden'
         } else {
             emailError.innerHTML = "<small>El email ingresado no corresponde con un email válido</small>"
+            emailError.style.visibility = 'visible'
         }
     })
     password.addEventListener('blur', function() {
         if(rePassword.value != "" && rePassword.value != password.value) {
             rePasswordError.innerHTML = "<small>Las contraseñas deben coincidir</small>"
+            rePasswordError.style.visibility = 'visible'
         }
         let passwordOk = regexPassword.test(password.value)
         if(passwordOk) {
             passwordError.innerHTML =""
+            passwordError.style.visibility = 'hidden'
         } else {
             passwordError.innerHTML = "<small>La contraseña debe tener al menos un número, una mayúscula, una mínuscula y 8 caracteres</small>"
+            passwordError.style.visibility = 'visible'
         }
     })
     rePassword.addEventListener('blur', function() {
         if(password.value != "" && rePassword.value != password.value) {
             rePasswordError.innerHTML = "<small>Las contraseñas deben coincidir</small>"
+            rePasswordError.style.visibility = 'visible'
         } else {
             rePasswordError.innerHTML = ""
+            rePasswordError.style.visibility = 'hidden'
         }
     })
     terms.addEventListener('change', function(){
         if(!terms.checked) { 
             termsError.innerHTML = "<small>Debes aceptar los términos y condiciones</small>"
+            termsError.style.visibility = 'visible'
         } else {
             termsError.innerHTML = ""
+            termsError.style.visibility = 'hidden'
         }
     })
 
@@ -81,26 +94,32 @@ window.addEventListener('load', function() {
 
         if(firstName.value.length < 2 ) {
             firstNameError.innerHTML = "<small>El nombre debe tener al menos 2 caracteres</small>"
+            firstNameError.style.visibility = 'visible'
             i++
         }
         if(lastName.value.length < 2 ) {
             lastNameError.innerHTML = "<small>El apellido debe tener al menos 2 caracteres</small>"
+            lastNameError.style.visibility = 'visible'
             i++
         }
         if(!emailOk) {
             emailError.innerHTML = "<small>El email ingresado no corresponde con un email válido</small>"
+            emailError.style.visibility = 'visible'
             i++
         }
         if(!passwordOk) {
             passwordError.innerHTML = "<small>La contraseña debe tener al menos un número, una mayúscula, una mínuscula y 8 caracteres</small>"
+            passwordError.style.visibility = 'visible'
             i++
         }
         if(rePassword.value != password.value) {
             rePasswordError.innerHTML = "<small>Las contraseñas deben coincidir</small>"
+            rePasswordError.style.visibility = 'visible'
             i++
         }
         if(!terms.checked) { 
             termsError.innerHTML = "<small>Debes aceptar los términos y condiciones</small>"
+            termsError.style.visibility = 'visible'
             i++
         }
         console.log(i);
