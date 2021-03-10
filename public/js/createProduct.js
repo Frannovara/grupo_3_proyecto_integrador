61adec727b0b8d6e63472178e5593eadef25df74
jflukac <juanfranciscolukac@gmail.com>
2021-03-10 10:53:41 -0300

    Create product validation finished

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -17,6 +17,7 @@ window.addEventListener('load', function(){
     let description = qs('#description')
     let image = qs('#image')
     let color = qs('#color')
+    let createForm = qs('.create-form')
     
     let nameError = qs('#nameError')
     let categoryError = qs('#categoryError')
@@ -108,6 +109,60 @@ window.addEventListener('load', function(){
         } else {
             colorError.innerHTML = `<small>Este campo es obligatorio</small>`
             colorError.style.visibility = "visible"
+        }
+    })
+
+    createForm.addEventListener('submit', function(event){
+        event.preventDefault()
+        let i = 0
+        if(name.value.length<5){
+            nameError.innerHTML = `<small>El nombre debe tener al menos 5 caracteres</small>`
+            nameError.style.visibility = "visible"
+            i++
+        }
+        if (!Number.isInteger(Number(category.value))) {
+            categoryError.innerHTML = `<small>Este campo es obligatorio</small>`
+            categoryError.style.visibility = "visible"
+        }
+        if (!Number.isInteger(Number(brand.value))) {
+            brandError.innerHTML = `<small>Este campo es obligatorio</small>`
+            brandError.style.visibility = "visible"
+            i++
+        }
+        if (!Number.isInteger(Number(price.value)) || Number(price.value) <= 0) {
+            priceError.innerHTML = `<small>El precio debe ser un número mayor a cero</small>`
+            priceError.style.visibility = "visible"
+            i++
+        }
+        if (!Number.isInteger(Number(discount.value)) || Number(discount.value) <= 0) {
+            discountError.innerHTML = `<small>Debe ingresar un valor de descuento</small>`
+            discountError.style.visibility = "visible"
+            i++
+        }
+        if (!Number.isInteger(Number(year.value)) || year.value.length != 4) {
+            yearError.innerHTML = `<small>Debe ingresar un año en formato YYYY</small>`
+            yearError.style.visibility = "visible"
+            i++
+        }
+        if(description.value.length <20) {
+            descriptionError.innerHTML = `<small>La descripción debe tener al menos 20 caracteres</small>`
+            descriptionError.style.visibility = "visible"
+            i++
+        }
+        let imageOk = regexImage.test(image.files[0]?.type)
+        if(!imageOk || image.files.length == 0) {
+            imageError.innerHTML = `<small>El archivo ingresado no es una imagen</small>`
+            imageError.style.visibility = "visible"
+            i++
+        }
+        if (!Number.isInteger(Number(color.value))) {
+            colorError.innerHTML = `<small>Este campo es obligatorio</small>`
+            colorError.style.visibility = "visible"
+            i++
+        }
+
+        if(i>0) {
+            event.preventDefault()
         }
     })
 
