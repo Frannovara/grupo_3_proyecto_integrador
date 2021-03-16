a6140f9b40ec56f3d0b52414436763e6e3760379
jflukac <juanfranciscolukac@gmail.com>
2021-03-16 14:59:28 -0300

    Fixed to fetch new API

--- public/js/newPassEmail.js
+++ public/js/newPassEmail.js
@@ -1,38 +1,35 @@
 window.addEventListener('load', ()=> {
 
-    fetch('/api/users')
-    .then(response => {
-        return response.json()
-    })
-    .then(dataDecode => {
-        console.log(dataDecode)
-        users = dataDecode.data;
-        console.log(users);
-    })
+    let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+
 
     let emailNewPass = document.querySelector('#email-newPass')
     let emailNewPassErrors = document.querySelector('#email-new-pass-errors')
     let form = document.querySelector('.new-pass-form')
 
-    emailNewPass.addEventListener('blur', function() {
-        console.log(users.indexOf(this.value));
-        if (users.indexOf(this.value) == -1 ) {
-            emailNewPassErrors.innerHTML = `<small>El email ingresado no corresponde con un email registrado</small>`
-            emailNewPassErrors.style.visibility = 'visible'
-        } else {
-            emailNewPassErrors.innerHTML = ``
-            emailNewPassErrors.style.visibility = 'hidden'
-        }
-    })
+
     form.addEventListener('submit', function(event) {
         event.preventDefault()
-        if(users.indexOf(emailNewPass.value) == -1) {
-            emailNewPassErrors.innerHTML = `<small>El email ingresado no corresponde con un email registrado</small>`
+        if(!emailNewPass.value.match(regexEmail)) {
+            emailNewPassErrors.innerHTML = `<small>El email ingresado no corresponde con un email válido</small>`
             emailNewPassErrors.style.visibility = 'visible'
         } else {
-            swal("Exito","Revise su email para regenerar su contraseña","success")
+            fetch(`/api/users/${emailNewPass.value}`)
+            .then(response => {
+                return response.json()
+            })
+            .then(dataDecode => {
+                let register = dataDecode.email;
+                console.log(register);
+                if (!register) {
+                    emailNewPassErrors.innerHTML = `<small>El email ingresado no corresponde con un email registrado</small>`
+                    emailNewPassErrors.style.visibility = 'visible'
+                } else {
+                    swal("Exito","Revise su email para regenerar su contraseña","success")
             .then( () => {
                 form.submit()
+            })
+                }
             })
         }
     })
