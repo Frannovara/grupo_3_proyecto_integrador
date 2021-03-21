ed12ffc2d6832bbc98fd8e36c1879aeb72c46662
lautaro porlan <l.porlann@gmail.com>
2021-03-21 14:53:20 -0300

    API userscontroller list method in process

--- src/controllers/api/usersController.js
+++ src/controllers/api/usersController.js
@@ -21,6 +21,27 @@ const controller = {
 
             return res.json(usuario)
         })
+    },
+    list: (req , res) =>{
+        db.Users.findAll()
+        .then(users =>{
+            
+            users = users.map(user => user = {id : user.id , first_name : user.first_name , last_name : user.last_name , email : user.email })
+
+            let usersResponse= {
+                meta : {
+                    status : 200,
+                    count : users.length
+                },
+                data : users
+            }
+            
+            res.json(usersResponse)
+        })     
+            
+            
+
+       
     }
 }
 
